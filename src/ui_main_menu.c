ffacebfdb4eef92b551def8cbee3726797005a39
Heather <hellisather@gmail.com>
2025-09-02 21:03:44 +0100

    Scrolling main menu to match bw summary

--- src/ui_main_menu.c
+++ src/ui_main_menu.c
@@ -410,8 +410,8 @@ static void MainMenu_VBlankCB(void)
     LoadOam();
     ProcessSpriteCopyRequests();
     TransferPlttBuffer();
-    ChangeBgY(2, 128, BG_COORD_SUB); // This controls the scrolling of the scroll bg, remove it to stop scrolling
-}
+    ChangeBgX(2, 64, BG_COORD_ADD);
+    ChangeBgY(2, 64, BG_COORD_ADD);}
 
 //
 //  Quit Out Functions
